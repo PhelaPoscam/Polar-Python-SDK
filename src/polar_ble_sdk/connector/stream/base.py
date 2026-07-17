2c307ca5cb8f8d51619d080f47b0c0ce1497ea71
Aleph Campos da Silveira <aleph.camposdasilveira@tuni.fi>
2026-07-17 09:51:39 +0300

    chore: add TODO notes for transitioning to pure bleak PMD decoders

--- src/polar_ble_sdk/connector/stream/base.py
+++ src/polar_ble_sdk/connector/stream/base.py
@@ -6,6 +6,7 @@ import sys
 import time
 import traceback
 from typing import Any
+# TODO: Replace polar_python dependency with native PMD protocol parsing and byte unpacking directly over pure bleak
 from polar_python import PolarDevice
 from polar_python.constants import (
     PmdMeasurementType,
